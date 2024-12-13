e76584c7cad1f1cf721430ce3b3ec90054c39456
Eren Kulaksız <erenkulaksz@gmail.com>
2024-12-14 00:37:38 +0300

    Initial Commit

---
+++ todo/todoApp.swift
@@ -0,0 +1,17 @@
+//
+//  todoApp.swift
+//  todo
+//
+//  Created by erenkulaksiz on 14.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct todoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
